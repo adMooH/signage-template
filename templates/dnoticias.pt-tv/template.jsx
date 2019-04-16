5e81c9d93d9cdf841b54fe0bdf5e8aae3f26efe8
Ademir Pereira <ademirspr@live.com>
2019-04-16 17:41:14 -0300

    Ajustes no template dnoticias portugal

--- templates/dnoticias.pt-tv/template.jsx
+++ templates/dnoticias.pt-tv/template.jsx
@@ -9,20 +9,22 @@ export default class DNoticiasNews extends React.Component {
 	}
 	render() {
 		const news = this._getNewsWithThumbnail();		
-		const type = news.source.__text;
+		const title = news.title;
+		const pic = news.content._url;
 		const dnew = news.description.replace(/(<([^>]+)>)/g, "").split('.')[0];
 		return (
 			<div style={layout.template_dnoticias}>
+				<DNoticiasLogo src={pic}/>
 				<div style={layout.dnoticias_logo}>
-					<img style={layout.dnoticias_logo_img} src={dnoticiasLogo} />
+					<div style={{...layout.dnoticias_logo_img,backgroundImage: `url(${pic || dnoticiasLogo})` }}/>
 				</div>
 				<div style={layout.news}>
-				<p style={layout.news_type} >
-					{type}
-				</p>
-				<p style={layout.news_text}>
-					{dnew.length > 157 ? dnew.substring(0,157) + "...": dnew + "."}
-				</p>					
+					<p style={layout.news_title} >
+						{title}
+					</p>
+					<p style={layout.news_text} >
+						{dnew.length > 130 ? dnew.substring(0,130) + "..." : dnew + "." }
+					</p>			
 				</div>
 			</div>
 		)
@@ -35,4 +37,33 @@ export default class DNoticiasNews extends React.Component {
 		const itemsLenght = arrayLength - 1;
 		return Math.floor(Math.random() * (itemsLenght - 0) + 0);
 	}
+}
+
+
+class DNoticiasLogo extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			shown: true
+		}
+	}
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({shown: false});
+		}, 3000);
+	}
+
+
+	render(){
+		const animation = this.state.shown ? {} 
+		:{
+			transition: 'opacity 0.5s linear',
+			opacity: 0
+		}
+		return (
+			<div style={{...layout.dnoticias_logo,zIndex: 9, ...animation}}>
+				<div style={{...layout.dnoticias_logo_img,backgroundImage: `url(${dnoticiasLogo})` }}/>
+			</div>
+		)
+	}	
 }
